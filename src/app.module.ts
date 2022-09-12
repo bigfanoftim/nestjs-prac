dc7cd5081f83ffe8ff41e7fda9c946268c4fc946
bigfanoftim <wkdghwns91@gmail.com>
2022-09-12 12:28:46 +0900

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from './logger.middleware';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
   imports: [CatsModule],
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -8,19 +8,22 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  UseInterceptors,
 } from '@nestjs/common';
 import { IDPositiveIntPipe } from 'src/common/IDPositiveInt.pipe';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 import { CreateCatDao } from './create-cat.dto';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
 export class CatsController {
   @Get()
-  findAll(): string {
-    throw new HttpException(
-      { statusCode: HttpStatus.FORBIDDEN, message: 'fail', error: 'test' },
-      HttpStatus.FORBIDDEN
-    );
-    return 'all cats';
+  findAll() {
+    // throw new HttpException(
+    //   { statusCode: HttpStatus.FORBIDDEN, message: 'fail', error: 'test' },
+    //   HttpStatus.FORBIDDEN
+    // );
+    return { cats: 'all cats' };
   }
 
   @Get(':id')
