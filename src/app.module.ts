0b7b3d1fecccd5818e7d5e458cc916b1d9455066
bigfanoftim <wkdghwns91@gmail.com>
2022-09-14 23:10:15 +0900

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
+import { UsersHttpModule } from './users-http/users-http.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'wkdghwns',
+      database: 'nest_prac',
+      entities: [User],
+      synchronize: true,
+    }),
+    CatsModule,
+    UsersModule,
+    UsersHttpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
